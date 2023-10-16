b4ace94a65ddde5d387fab425e148fec6fc773cf
Nicholas <nicholasdragone@gmail.com>
2023-10-16 15:25:22 -0400

    complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.getElementById('create-task-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+     const taskValue = document.getElementById('new-task-description').value;
+     if (taskValue.trim() !== "") {
+        const listItem = document.createElement('li');
+        listItem.textContent = taskValue;
+        document.getElementById('tasks').appendChild(listItem);
+        document.getElementById('new-task-description').value = "";
+    }
 });
